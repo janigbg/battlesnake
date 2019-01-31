9ca3384838cc4d4fc8be513fe2eb8710f5db233d
Jani Suihko <jani.suihko.ext@toyota-industries.eu>
2019-01-31 15:29:54 +0100

    Hungry snake

--- netcore-boilerplate/NetSnake/SnakeController.cs
+++ netcore-boilerplate/NetSnake/SnakeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Linq;
 using AStarNavigator;
+using AStarNavigator.Algorithms;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 using NetSnake.Model;
 using NetSnake.Snake;
 
@@ -30,8 +32,35 @@ namespace NetSnake
             GameBoard = new GameBoard(request.you, request.board);
             GameBoard.Update(request.you, request.board);
 
-            // Undvik väggar
             var head = request.you.body.First();
+            var heuristic = new ManhattanHeuristicAlgorithm();
+            TileNavigator navigator = new TileNavigator(
+                GameBoard,
+                GameBoard,
+                new PythagorasAlgorithm(),
+                heuristic);
+
+            int dist = Int32.MaxValue;
+            Tile? foodTile = null;
+            foreach (var food in request.board.food)
+            {
+                var newDist = heuristic.Calculate(ToTile(head), ToTile(food));
+                if (newDist< dist)
+                {
+                    dist = (int)newDist;
+                    foodTile = ToTile(food);
+                }
+            }
+
+            if (foodTile.HasValue)
+            {
+                var startPath = navigator.Navigate(ToTile(head), foodTile.Value).FirstOrDefault();
+                if (startPath != null && GetMovement(startPath, head, out var foodMove)) return foodMove;
+            }
+
+
+            // Undvik väggar
+            
 
             var tiles = GameBoard.GetNeighbors(new Tile(head.x, head.y)).Where(x => !GameBoard.IsBlocked(x)).ToList();
 
@@ -45,19 +74,44 @@ namespace NetSnake
 
             var tile = tiles[index];
 
+            if (GetMovement(tile, head, out var move)) return move;
+
+            return Ok(new Move());
+        }
+
+        private bool GetMovement(Tile tile, Coords head, out IActionResult move)
+        {
             switch (tile)
             {
                 case Tile t when t.Y < head.y:
-                    return Ok(new Move { Direction = Direction.Up });
+                {
+                    move = Ok(new Move {Direction = Direction.Up});
+                    return true;
+                }
                 case Tile t when t.Y > head.y:
-                    return Ok(new Move { Direction = Direction.Down });
+                {
+                    move = Ok(new Move {Direction = Direction.Down});
+                    return true;
+                }
                 case Tile t when t.X < head.x:
-                    return Ok(new Move { Direction = Direction.Left });
+                {
+                    move = Ok(new Move {Direction = Direction.Left});
+                    return true;
+                }
                 case Tile t when t.X > head.x:
-                    return Ok(new Move { Direction = Direction.Right });
+                {
+                    move = Ok(new Move {Direction = Direction.Right});
+                    return true;
+                }
             }
 
-            return Ok(new Move());
+            move = Ok(new Move());
+            return false;
+        }
+
+        private static Tile ToTile(Coords coords)
+        {
+            return new Tile(coords.x, coords.y);
         }
 
         [HttpPost]
